bc0ef51947d23b06788be66f6bf5f893fa63f3d9
Björn Büttner <Idrinth@users.noreply.github.com>
2017-11-18 22:04:55 +0100

    reducing runntime of tests

--- src/test/java/de/idrinth/name_generator/implementation/DataTest.java
+++ src/test/java/de/idrinth/name_generator/implementation/DataTest.java
@@ -11,7 +11,7 @@ public class DataTest {
     private String getLongRandomString() {
         String[] letters = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzäöüß".split("");
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 50; i++) {
             sb.append(letters[(int) Math.floor(Math.random() * letters.length)]);
         }
         return sb.toString();
@@ -37,6 +37,7 @@ public class DataTest {
     public void testInitialisationFromFile() {
         System.out.println("initialisation from file");
         DataAccess instance = new DataAccess();
+        assertNotNull(instance);
         instance.getCounts().forEach((check) -> {
             assertTrue(check);
         });
